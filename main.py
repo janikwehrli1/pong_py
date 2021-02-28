31392b0920af470df4512228fb26010b3a2c86e2
janikwibilea <janik.wehrli@bbz-sh.ch>
2021-02-28 12:41:08 +0100

    scoreboard.py

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
 from turtle import Turtle, Screen
 from players import Player
+from ball import Ball
+import time
+from scoreboard import Scoreboard
 
 myscreen = Screen()
 myscreen.bgcolor("black")
 myscreen.setup(width=800, height=600)
 myscreen.tracer(0)
 
+myscoreboard = Scoreboard()
 
-newplayer = Player()
+r_player = Player((350, 0))
+l_player = Player((-350, 0))
+ball = Ball()
 
 myscreen.listen()
-myscreen.onkeypress(newplayer.go_up, "Up")
-myscreen.onkeypress(newplayer.go_down, "Down")
+myscreen.onkeypress(r_player.go_up, "Up")
+myscreen.onkeypress(r_player.go_down, "Down")
+
+myscreen.onkeypress(l_player.go_up, "w")
+myscreen.onkeypress(l_player.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     myscreen.update()
+    ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
 
+    if ball.distance(r_player) < 50 and ball.xcor() > 320 or ball.distance(l_player) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        myscoreboard.l_point()
+    if ball.xcor() < -380:
+        ball.reset_position()
+        myscoreboard.r_point()
 
 myscreen.exitonclick()
--- players.py
+++ players.py
@@ -2,14 +2,13 @@ from turtle import Turtle
 
 
 class Player(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.color("white")
         self.shape("square")
         self.penup()
         self.turtlesize(stretch_len=1, stretch_wid=5)
-        self.goto(x=350, y=0)
-
+        self.goto(position)
 
     def go_up(self):
         new_y = self.ycor() + 20
